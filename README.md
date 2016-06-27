cc4e33687a2d124fc5f8702a01cdb5690d387095
Mark Steve Samson <hello@marksteve.com>
2016-06-27 14:09:58 +0800

    Update with message buttons

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# slash-deploybot
+# slash-spoilers
 
-Slack slash command for DeployBot
+Slack slash command for spoilers
 
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
 
--- app.py
+++ app.py
@@ -1,29 +1,92 @@
 from os import environ
+from uuid import uuid4
 
 import requests
-from flask import abort, Flask, request
+from flask import Flask, json, jsonify, redirect, request
 
 application = app = Flask(__name__)
+client_id = environ['SLACK_CLIENT_ID']
+client_secret = environ['SLACK_CLIENT_SECRET']
+# verification_token = environ['SLACK_VERIFICATION_TOKEN']
 
-slack = requests.Session()
-slack.params.update({
-    'token': environ['SLACK_API_TOKEN'],
-})
-slash_token = environ['SLACK_SLASH_COMMAND_TOKEN']
-
-
-def get_slack_users():
-    return {
-        user['id']: user for user in
-        slack.get('https://slack.com/api/users.list').json()['members']
-    }
+tokens = {}
+spoilers = {}
 
 
-@app.route('/', methods=['POST'])
-def handler():
-    if request.form['token'] != slash_token:
-        abort(403)
-    return "SPOILERS"
+@app.route('/auth')
+def auth():
+    return redirect('https://slack.com/oauth/authorize', params={
+        'client_id': client_id,
+        'scope': 'commands chat:write:user',
+    })
+
+
+@app.route('/oauth')
+def oauth():
+    code = request.args['code']
+    r = requests.post('https://slack.com/api/oauth.access', json={
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': code,
+    })
+    data = r.json()
+
+    token = data['access_token']
+    r = requests.post('https://slack.com/api/auth.test', json={
+        'token': token,
+    })
+    data = r.json()
+    tokens[data['user_id']] = token
+
+    return jsonify({'ok': True})
+
+
+@app.route('/command', methods=['POST'])
+def command():
+    channel = request.form['channel_id']
+    user = request.form['user_id']
+    user_name = request.form['user_name']
+    spoiler = request.form['text']
+    token = tokens[user]
+
+    callback_id = uuid4()
+    spoilers[callback_id] = spoiler
+
+    requests.post('https://slack.com/api/chat.postMessage', json={
+        'token': token,
+        'channel': channel,
+        'as_user': True,
+        'attachments': [
+            {
+                'text': '{} wrote a spoiler!'.format(user_name),
+                'fallback': '{} wrote a spoiler!'.format(user_name),
+                'callback_id': callback_id,
+                'actions': [
+                    {
+                        'name': 'show_spoiler',
+                        'text': 'Show spoiler',
+                        'type': 'button',
+                        'confirm': {
+                            'title': 'Are you sure?',
+                            'text': 'You will be spoiled!',
+                            'ok_text': 'Bring it on',
+                            'dismiss_text': 'Nope',
+                        },
+                    }
+                ],
+            }
+        ],
+        'response_type': 'ephemeral',
+    })
+
+    return jsonify({'ok': True})
+
+
+@app.route('/interact', methods=['POST'])
+def interact():
+    data = json.loads(request.form['payload'])
+    spoiler = spoilers[data['callback_id']]
+    return jsonify({'text': spoiler})
 
 
 if __name__ == '__main__':
